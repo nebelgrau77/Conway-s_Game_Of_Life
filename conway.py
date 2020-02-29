fdcbba86dd5ec9c6f8cf2197441324869b50e470
Michal <36729174+nebelgrau77@users.noreply.github.com>
2020-02-29 22:44:51 +0100

    Add files via upload

---
+++ conway.py
@@ -0,0 +1,81 @@
+'''Conway's Game of Life draft'''
+
+import machine, ssd1306, uos
+
+# display setup
+
+i2c = machine.I2C(scl=machine.Pin(21), sda=machine.Pin(22))
+oled = ssd1306.SSD1306_I2C(128,32,i2c,0x3c)
+
+# helper functions
+
+def randomcell():
+    '''generate random 1s and 0s'''
+    return uos.urandom(1)[0]%2
+
+
+def evo(cell, neighbors):
+    '''generate the new cell based on it's neighbors and the cell's initial state'''
+    
+    new_cell = 0
+    if cell == 1:
+        if neighbors in [2,3]:
+            new_cell = 1 # lives        
+    else:
+        if neighbors == 3:
+            new_cell = 1 # it takes three to give birth        
+    return new_cell    
+
+
+def matrix_evo(matrix, size):
+    '''returns a new matrix calculated according to the rules of the Game of Life'''
+    new_matrix = []
+    for x in range(size):
+        for y in range(size):
+            items = []
+            for n in [-1,0,1]:
+                for m in [-1,0,1]:
+                    if x + n < 0 or y + m < 0 or x + n > size - 1 or y + m > size - 1 or m == n == 0:
+                        items.append(0)
+                    else:
+                        items.append(matrix[x+n][y+m])
+                    cell = matrix[x][y]
+                    neighbors = sum(items)
+                    new_cell = evo(cell, neighbors)
+            new_matrix.append((x,y,new_cell))
+    return new_matrix
+
+
+def matrix_update(matrix, new_matrix):
+    '''update the previous matrix'''
+    for item in new_matrix:
+        matrix[item[0]][item[1]] = item[2]
+    return matrix
+
+
+def display_matrix(matrix, size):
+    for x in range(size):
+        for y in range(size):
+            oled.pixel(x,y,matrix[x][y])
+    oled.show()
+
+
+
+
+# first matrix
+
+matrix = [[randomcell() for _ in range(32)] for _ in range(32)]
+
+# clean up the screen
+oled.show()
+
+display_matrix(matrix,32)
+
+
+while True:
+    new_matrix = matrix_evo(matrix,32)
+    
+    matrix = matrix_update(matrix, new_matrix)
+    
+    display_matrix(matrix,32)
+    
